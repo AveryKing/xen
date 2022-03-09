6e54feb2ff39a696d257ef13f99b25c14cbc5979
Avery King <avery.n.king@wmich.edu>
2022-03-09 01:15:35 -0500

    password vaidatino added

--- functions/index.js
+++ functions/index.js
@@ -43,6 +43,12 @@ app.post('/post', (req, res) => {
         })
 });
 
+const isEmpty = (string) => !string.trim().length;
+
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return email.match(regEx);
+}
 // User registration
 app.post('/register', (req, res) => {
     const newUser = {
@@ -51,7 +57,29 @@ app.post('/register', (req, res) => {
         confirmPassword: req.body.confirmPassword,
         handle: req.body.handle
     };
-    //TODO: validate data
+
+    const errors = {};
+    const userDataArray = Object.entries(newUser);
+    userDataArray.forEach(kv => {
+        const currentIndex = userDataArray.indexOf(kv)
+        const [key, value] = kv;
+        if (isEmpty(value)) errors[key] = 'this field cannot be empty';
+        switch(key) {
+            case 'email':
+                if(!isEmail) errors[key] = 'that email is invalid';
+                break;
+            case 'handle':
+                if(value.length < 3) errors[key] = 'username must be at least 3 characters';
+                break;
+            case 'password':
+                if(value !== userDataArray[currentIndex+1][1]) {
+                    errors[key] = 'the two passwords you entered do not match';
+                }
+        }
+    })
+
+    if(Object.keys(errors).length) return res.status(400).json(errors);
+
     db.doc(`/users/${newUser.handle}`)
         .get()
         .then(doc => {
@@ -73,14 +101,14 @@ app.post('/register', (req, res) => {
                         return db.doc(`/users/${newUser.handle}`).set(userData)
                     })
                     .then(() => {
-                        res.status(201).json({message: `user '${newUser.handle}' created successfully`});
+                        return res.status(201).json({message: `user '${newUser.handle}' created successfully`});
                     })
                     .catch(err => {
                         console.error(err);
                         if (err.code === 'auth/email-already-in-use') {
-                            res.status(400).json({email: 'email is already in use'});
+                           return res.status(400).json({email: 'email is already in use'});
                         } else {
-                            res.status(500).json({error: err.code})
+                            return res.status(500).json({error: err.code})
                         }
                     })
 
