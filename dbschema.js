74e8decb0b216c51164e3df4bf4a9b816e4f96c0
Avery King <avery.n.king@wmich.edu>
2022-03-08 22:36:17 -0500

    dbschema.js added

---
+++ dbschema.js
@@ -0,0 +1,12 @@
+// schema structure; for reference only:
+let db = {
+    posts: [
+        {
+            userHandle: 'avery',
+            body: 'post body',
+            createdAt: '2022-03-08T11:46:01.018Z',
+            likeCount: 11,
+            commentCount: 6
+        }
+    ]
+}
