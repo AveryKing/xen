ca30cf5c0fb5784d5d91d7cfeb4cd5a6aabd7ad5
Avery King <avery.n.king@wmich.edu>
2022-03-10 18:50:08 -0500

    ``createNotificationOnComment`` implemented

--- functions/index.js
+++ functions/index.js
@@ -37,6 +37,7 @@ app.get('/user', FBAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
 
+//TODO: condense createNotificationOnLike and createNotificationOnComment into one function
 exports.createNotificationOnLike = functions.firestore.document(`likes/{id}`)
     .onCreate((snapshot) => {
         db.doc(`/posts/${snapshot.data().postId}`).get()
@@ -55,4 +56,25 @@ exports.createNotificationOnLike = functions.firestore.document(`likes/{id}`)
             .catch(err => {
                 console.error(err);
             })
-    })
+    });
+
+exports.createNotificationOnComment = functions.firestore.document('comments/{id}')
+    .onCreate((snapshot) => {
+        db.doc(`/posts/${snapshot.data().postId}`).get()
+            .then(doc => {
+                if(doc.exists) {
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'comment',
+                        read: false,
+                        postId: doc.id
+                    })
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    });
+
