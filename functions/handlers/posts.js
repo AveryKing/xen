56658c23b34efb054fe3be48a5b7823fcaa15052
Avery King <avery.n.king@wmich.edu>
2022-03-10 17:05:30 -0500

    ``likePost`` and ``unlikePost`` apis added

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -92,7 +92,7 @@ exports.commentOnPost = (req, res) => {
 
     db.doc(`/posts/${req.params.postId}`).get()
         .then(doc => {
-            if(!doc.exists) {
+            if (!doc.exists) {
                 return res.status(404).json({error: 'Post does not exist'});
             }
             return db.collection('comments').add(newComment);
@@ -106,10 +106,91 @@ exports.commentOnPost = (req, res) => {
         })
 }
 
-exports.likePost = (req,res) => {
-    res.json({});
+// Like a post
+exports.likePost = (req, res) => {
+    const likeDoc = db.collection('likes')
+        .where('userHandle', '==', req.user.handle)
+        .where('postId', '==', req.params.postId)
+        .limit(1)
+
+    const postDoc = db.doc(`posts/${req.params.postId}`);
+    let postData;
+
+    postDoc.get()
+        .then(doc => {
+            if (doc.exists) {
+                postData = doc.data();
+                postData.postId = doc.id;
+                return likeDoc.get();
+            } else {
+                return res.status(404).json({error: 'Post does not exist'});
+            }
+        })
+        .then(data => {
+            if (data.empty) {
+                // Not yet liked
+                return db.collection('likes').add({
+                    postId: req.params.postId,
+                    userHandle: req.user.handle
+                })
+                    .then(() => {
+                        postData.likeCount++;
+                        return postDoc.update({likeCount: postData.likeCount});
+                    })
+                    .then(() => {
+                        return res.json(postData);
+                    })
+            } else {
+                // Already liked
+                return res.status(400).json({error: 'Post already liked'});
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({error: err.code})
+        })
+
 }
 
-exports.unlikePost = (req,res) => {
-    res.json({});
+// Unlike a post
+exports.unlikePost = (req, res) => {
+    const likeDoc = db.collection('likes')
+        .where('userHandle', '==', req.user.handle)
+        .where('postId', '==', req.params.postId)
+        .limit(1)
+
+    const postDoc = db.doc(`posts/${req.params.postId}`);
+    let postData;
+
+    postDoc.get()
+        .then(doc => {
+            if (doc.exists) {
+                postData = doc.data();
+                postData.postId = doc.id;
+                return likeDoc.get();
+            } else {
+                return res.status(404).json({error: 'Post does not exist'});
+            }
+        })
+        .then(data => {
+            if (data.empty) {
+                // Not yet liked
+                return res.status(400).json({error: 'Post already liked'});
+
+
+            } else {
+                db.doc(`/likes/${data.docs[0].data().id}`).delete()
+                    .then(() => {
+                        postData.likeCount--;
+                        return postDoc.update({likeCount: postData.likeCount});
+                    })
+                    .then(() => {
+                        return res.json(postData);
+                    })
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({error: err.code})
+        })
 }
