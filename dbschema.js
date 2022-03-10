f8de59b349604bea758d9d21aeb3fd972588cdd8
Avery King <avery.n.king@wmich.edu>
2022-03-09 23:42:46 -0500

    ``dbschema.js`` update

--- dbschema.js
+++ dbschema.js
@@ -1,5 +1,17 @@
 // schema structure; for reference only:
 let db = {
+    users: [
+        {
+            userId: 'fCvydo0mcSV3lsIciK1DjSqFBWQ2',
+            email: 'avery@fbi.gov',
+            handle:'king',
+            createdAt: '2022-03-08T11:46:01.018Z',
+            imageUrl: 'https://firebasestorage.googleapis.com/v0/b/zensocial-501c5.appspot.com/o/1122608.png?alt=media',
+            bio: 'Hello, my name is Avery',
+            website:'https://fbi.gov',
+            location: 'Heaven, Earth'
+        }
+    ],
     posts: [
         {
             userHandle: 'avery',
