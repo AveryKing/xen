05fee836f5211eb859f5e6fb0377a63c80011d8d
Avery King <avery.n.king@wmich.edu>
2022-03-09 21:41:40 -0500

    authorization middleware added

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
 const functions = require("firebase-functions");
+const firebase = require('firebase/app');
 const app = require('express')();
+const {getAuth, getIdToken, signInWithEmailAndPassword} = require("firebase/auth");
 const admin = require("firebase-admin");
 admin.initializeApp();
-
+const firebaseConfig = {
+    apiKey: "AIzaSyDQK_HCnOAHoQ7YA_rJOyesA--e3lvrrTA",
+    authDomain: "zensocial-501c5.firebaseapp.com",
+    projectId: "zensocial-501c5",
+    storageBucket: "zensocial-501c5.appspot.com",
+    messagingSenderId: "295815274601",
+    appId: "1:295815274601:web:0a2e829b3d4fa4e94d82f8",
+    measurementId: "G-54DJT2WTHJ"
+};
+firebase.initializeApp(firebaseConfig);
 
 const db = admin.firestore();
 
@@ -24,8 +35,24 @@ app.get('/posts', (req, res) => {
         .catch(err => console.error);
 });
 
+// Authorization middleware
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found')
+        return res.status(403).json({error: 'unauthorized'});
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+        })
+}
+
 // Creates new post
-app.post('/post', (req, res) => {
+app.post('/post', FBAuth, (req, res) => {
     const newPost = {
         userHandle: req.body.userHandle,
         body: req.body.body,
@@ -49,6 +76,8 @@ const isEmail = (email) => {
     const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return email.match(regEx);
 }
+
+
 // User registration
 app.post('/register', (req, res) => {
     const newUser = {
@@ -64,21 +93,21 @@ app.post('/register', (req, res) => {
         const currentIndex = userDataArray.indexOf(kv)
         const [key, value] = kv;
         if (isEmpty(value)) errors[key] = 'this field cannot be empty';
-        switch(key) {
+        switch (key) {
             case 'email':
-                if(!isEmail) errors[key] = 'that email is invalid';
+                if (!isEmail) errors[key] = 'that email is invalid';
                 break;
             case 'handle':
-                if(value.length < 3) errors[key] = 'username must be at least 3 characters';
+                if (value.length < 3) errors[key] = 'username must be at least 3 characters';
                 break;
             case 'password':
-                if(value !== userDataArray[currentIndex+1][1]) {
+                if (value !== userDataArray[currentIndex + 1][1]) {
                     errors[key] = 'the two passwords you entered do not match';
                 }
         }
     })
 
-    if(Object.keys(errors).length) return res.status(400).json(errors);
+    if (Object.keys(errors).length) return res.status(400).json(errors);
 
     db.doc(`/users/${newUser.handle}`)
         .get()
@@ -106,7 +135,7 @@ app.post('/register', (req, res) => {
                     .catch(err => {
                         console.error(err);
                         if (err.code === 'auth/email-already-in-use') {
-                           return res.status(400).json({email: 'email is already in use'});
+                            return res.status(400).json({email: 'email is already in use'});
                         } else {
                             return res.status(500).json({error: err.code})
                         }
@@ -116,5 +145,33 @@ app.post('/register', (req, res) => {
 
         });
 });
+
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+    const errors = {};
+    if (isEmpty(user.email)) errors.email = 'Must not be empty';
+    if (isEmpty(user.password)) errors.password = "Must not be empty";
+    if (Object.keys(errors).length) return res.status(400).json(errors);
+
+    signInWithEmailAndPassword(getAuth(), user.email, user.password)
+        .then(data => {
+            return getIdToken(data.user);
+        })
+        .then(token => {
+            return res.json({token})
+        })
+        .catch(err => {
+            console.error(err);
+            if (err.code === 'auth/wrong-password') {
+                return res.status(403)
+                    .json({general: "Wrong password"});
+            } else {
+                return res.status(500).json({error: err.code});
+            }
+        })
+})
 
 exports.api = functions.https.onRequest(app);
