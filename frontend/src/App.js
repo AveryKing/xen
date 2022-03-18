65b62e9c41c48e7f5d845189a93a46cb66dfc8ba
Avery King <avery.n.king@wmich.edu>
2022-03-18 01:19:39 -0400

    ``Navbar`` added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,11 +8,11 @@ import {
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import WithSubnavigation from './components/Navbar'
+import Navbar from './components/Navbar'
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <WithSubnavigation />
+      <Navbar />
       <Router>
         <Switch>
           <Route exact path='/' component={Home}/>
