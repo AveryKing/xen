1e23d0975e436e9a349a5d83ef1641ae87382d7f
Avery King <avery.n.king@wmich.edu>
2022-03-21 23:08:04 -0400

    button href fix

--- xen-client/components/CallToAction.tsx
+++ xen-client/components/CallToAction.tsx
@@ -8,7 +8,7 @@ const CallToAction = () => {
                 <h2 className='lg:text-lg md:pl-10'>Tired of mindlessly scrolling on platforms such as Facebook and Twitter?
                     Here at Xen, you can explore content that will actively stimulate and engage your mind.
                     Create a free account now to make meaningful connections and mindfully flow.</h2>
-                <button href='#' className='hover:bg-pink-400  border w-32 px-4 mx-auto py-1 rounded-full bg-pink-500 text-white border-pink-500'>Join Now</button>
+                <button className='hover:bg-pink-400  border w-32 px-4 mx-auto py-1 rounded-full bg-pink-500 text-white border-pink-500'>Join Now</button>
 
             </div>
             <div className='hidden md:w-5/12 md:inline-flex p-10 lg:mr-5 object-contain'>
