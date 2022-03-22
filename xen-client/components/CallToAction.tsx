91aad8dceb37f5620968424bfe84c5542168595c
Avery King <avery.n.king@wmich.edu>
2022-03-21 21:24:40 -0400

    ubuntu font added

---
+++ xen-client/components/CallToAction.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CallToAction = () => {
+    return (
+        <div className='flex mx-auto justify-center mt-16 text-4xl px-5 text-center w-50'>
+            <h1>Tired of Mindlessly Scrolling?</h1>
+        </div>
+    );
+};
+
+export default CallToAction;
--- xen-client/pages/index.tsx
+++ xen-client/pages/index.tsx
@@ -1,12 +1,22 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from "../components/Navbar";
+import CallToAction from "../components/CallToAction";
 
 const Home: NextPage = () => {
-  return (
-    <Navbar />
-  )
+    return (
+        <>
+            <Head>
+                <title>Xen</title>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
+            </Head>
+            <Navbar/>
+            <CallToAction/>
+        </>
+    )
 }
 
 export default Home
