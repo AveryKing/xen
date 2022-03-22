f184097d5a73c7b4474a059838cb905f9fac9dd1
Avery King <avery.n.king@wmich.edu>
2022-03-21 22:22:52 -0400

    cta styled

--- xen-client/components/CallToAction.tsx
+++ xen-client/components/CallToAction.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const CallToAction = () => {
     return (
-        <div className='flex mx-auto justify-center mt-16 text-4xl px-5 text-center w-50'>
-            <h1>Tired of Mindlessly Scrolling?</h1>
+        <div className='flex justify-between  items-center bg-pink-50  py-10 lg:py-0'>
+            <div className='px-5 lg:px-10 space-y-5 text-center lg:mb-10'>
+                <h1 className='text-4xl md:text-5xl lg:text-6xl md:pl-10 '>Find Your Flow</h1>
+                <h2 className='lg:text-lg md:pl-10'>Tired of mindlessly scrolling on platforms such as Facebook and Twitter?
+                    Here at Xen, you can explore content that will actively stimulate and engage your mind.
+                    Create a free account now to make meaningful connections and mindfully flow.</h2>
+            </div>
+            <img className='hidden md:inline-flex flex-grow p-10 md:w-96 lg:w-full lg:mr-5 object-contain'
+                 src="cta.png"
+                 alt="Medusa"/>
         </div>
     );
 };
