78e216745defa45e5113f0b2bcbaa3e383cd6eb2
Avery King <avery.n.king@wmich.edu>
2022-03-22 02:46:38 -0400

    animations added

--- xen-client/components/Navbar.js
+++ xen-client/components/Navbar.js
@@ -1,24 +1,53 @@
 import {GiYinYang, GiHamburgerMenu} from 'react-icons/gi'
+import {motion} from "framer-motion";
+
 const Navbar = () => {
+    const fadeIn = {
+        initial: {opacity: 0},
+        animate: {opacity: 1},
+        transition: {duration: 1, type: 'tween', ease: 'anticipate'}
+    }
+    const MotionLogo = motion(GiYinYang);
     return (
         <header className='font-ubuntu flex justify-between px-8 py-3 max-w-full mx-auto shadow-md'>
             <div className="flex items-center space-x-5">
-                <div className={'flex items-center space-x-2 text-2xl'}>
-                    <a href={'/'}>
-                        <GiYinYang className={'text-4xl text-pink-500'}/>
-                    </a>
-                    <p className='font-logo font-semibold lg:text-3xl'>Xen</p>
+                <div
+                    className={'flex items-center space-x-2 text-2xl'}>
+                    <motion.a
+                        initial={{rotate: 0}}
+                        animate={{rotate: 360}}
+                        transition={{duration: 10}}
+                        href={'/'}>
+                        <GiYinYang
+                            className={'text-4xl text-pink-500'}/>
+                    </motion.a>
+                    <motion.p
+                        initial={{opacity: 0}}
+                        animate={{opacity: 1}}
+                        transition={{duration: 1.5}}
+                        className='font-logo font-semibold lg:text-3xl'>Xen
+                    </motion.p>
                 </div>
-                <div className=" hidden md:inline-flex items-center space-x-5">
+                <motion.div
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    transition={{duration: 1.5}}
+                    className=" hidden md:inline-flex items-center space-x-5">
                     <h3>Home</h3>
                     <h3>Explore</h3>
-                    <h3 className="text-black">About</h3>
-                </div>
+                    <h3>About</h3>
+                </motion.div>
             </div>
-            <div className='flex items-center space-x-5 text-pink-500'>
+            <motion.div
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                transition={{duration: 1.5}} className='flex items-center space-x-5 text-pink-500'>
                 <h3>Sign In</h3>
-                <button className='hover:bg-pink-400 border px-4 py-1 rounded-full bg-pink-500 text-white border-pink-500'>Get Started</button>
-            </div>
+                <button
+                    className='hover:scale-105 hover:bg-pink-400 border px-4 py-1 rounded-full bg-pink-500 text-white border-pink-500'>Get
+                    Started
+                </button>
+            </motion.div>
         </header>
     )
 }
