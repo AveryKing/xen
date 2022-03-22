68ecf6eab5f1c25828ca7820e024b84556bee887
Avery King <avery.n.king@wmich.edu>
2022-03-21 21:29:05 -0400

    Ubuntu font added to ``tailwind.config.js``

--- xen-client/pages/_document.tsx
+++ xen-client/pages/_document.tsx
@@ -9,7 +9,12 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head />
+                <Head>
+                    <title>Xen</title>
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
+                </Head>
                 <body>
                 <Main />
                 <NextScript />
