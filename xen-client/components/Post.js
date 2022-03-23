ce6db9e8e08c7b8e4d6c701f50aa2c6d93dba921
Avery King <avery.n.king@wmich.edu>
2022-03-23 02:52:51 -0400

    icons/date added

--- xen-client/components/Post.js
+++ xen-client/components/Post.js
@@ -1,27 +1,47 @@
 import React from 'react';
-import {Avatar} from "@chakra-ui/react";
+import {Avatar,IconButton} from "@chakra-ui/react";
+import {BiComment,BiRepost} from "react-icons/bi";
+import {BsHeart, BsHeartFill, BsStar, BsStarFill} from "react-icons/bs";
 
 const Post = () => {
     return (
         <>
             <div className='rounded-lg shadow border w-auto max-w-md  h-auto p-5'>
                 <div className='flex items-center space-x-2'>
-                    <Avatar size='sm'/>
-                    <p>Avery King</p>
+                    <Avatar className='mt-1' size='sm'/>
+                    <div className='block '>
+                        <p>Avery King</p>
+                        <p className='text-xs'>December 21, 2021</p>
+                    </div>
+
+
                 </div>
+
                 <div className='mx-auto mt-2'>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias, praesentium? Cum, deleniti dolorum eius facilis fuga minima mollitia natus necessitatibus odit quaerat quam qui quis rerum suscipit tempore voluptas.</p>
                 </div>
-                <div className='w-full hover:bg-gray-50 mt-3 -mb-3 text-sm'>
-                    <div>
+                <div className='w-full mt-3 -mb-3 text-sm'>
+                <IconButton
 
-                    </div>
-                    <div>
+                        variant={'ghost'}
+                        color='#de2852'
+                        icon={<BsHeartFill size={23}/>}
+                        aria-label={'heart'}/>
+                <IconButton
+                    variant={'ghost'}
+                    color='#f5c400'
+                    icon={<BsStarFill size={25}/>}
+                    aria-label={'star'}/>
+                <IconButton
+                    variant={'ghost'}
+                    icon={<BiComment/>}
+                    aria-label={'comment'}/>
+                    <IconButton
+                        variant={'ghost'}
 
-                    </div>
-                    <div>
-
-                    </div>
+                        fontSize={20}
+                        icon={<BiRepost/>}
+                        aria-label={'repost'}/>
                 </div>
             </div>
         </>
